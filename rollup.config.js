9ca9d3f82268e2abf27f73f204884a9764f29ee4
Jorge Zambrano <jorgemndz994@gmail.com>
2024-01-17 22:43:15 -0500

    Plugins installed

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-css-only';
 
 const config = {
   input: 'src/index.js',
   output: {
     dir: 'dist',
   },
-  plugins: [resolve(), commonjs(), babel({ babelHelpers: 'bundled' })],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({ babelHelpers: 'bundled' }),
+    css({ output: 'bundle.css' }),
+  ],
 };
 
 export default config;
